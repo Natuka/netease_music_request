35047c714381a36ee7e1f2d21c24c74b48a82dba
aihailin <alan1766447919@gmail.com>
2020-06-21 13:30:00 +0800

    add. cookie dir

--- example/login.dart
+++ example/login.dart
@@ -2,7 +2,7 @@
 import 'package:netease_music_request/request.dart';
 
 void main() {
-  var phone = '';
-  var password = '';
+  var phone = '19974168895';
+  var password = 'Alan147123369';
   User().loginByPhone(phone, password).then((data) => print(data));
 }
